1c6e7333e78871e20b98e9314837c27add814bb6
Ben Lavery-Griffiths <ben@lavery-griffiths.com>
2025-02-26 11:25:00 +0000

    Initial development

---
+++ config/config.go
@@ -0,0 +1,83 @@
+package config
+
+import (
+	logger "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"runtime"
+	"sync"
+)
+
+var (
+	once   sync.Once
+	config Config
+)
+
+type Config struct {
+	GoCardless GoCardlessConfig `yaml:"gocardless"`
+	Pushover   PushoverConfig   `yaml:"pushover"`
+}
+
+type GoCardlessConfig struct {
+	Credentials CredentialsConfig `yaml:"credentials"`
+	Bank        BankConfig        `yaml:"bank"`
+}
+
+type CredentialsConfig struct {
+	SecretID  string `yaml:"secretID"`
+	SecretKey string `yaml:"secretKey"`
+}
+
+type BankConfig struct {
+	Institution string        `yaml:"institution"`
+	Accounts    []BankAccount `yaml:"accounts"`
+}
+
+type BankAccount struct {
+	ID             string  `yaml:"id"`
+	Name           string  `yaml:"name"`
+	BalanceType    string  `yaml:"balanceType"`
+	Balance        float64 `yaml:"balance,omitempty"`
+	Currency       string  `yaml:"currency,omitempty"`
+	CurrencySymbol string  `yaml:"currencySymbol,omitempty"`
+}
+
+type PushoverConfig struct {
+	Tokens PushoverTokens `yaml:"tokens"`
+}
+
+type PushoverTokens struct {
+	User string `yaml:"user"`
+	App  string `yaml:"app"`
+}
+
+func GetConfig() Config {
+	once.Do(func() {
+		logger.Debug("Reading config file")
+		viper.SetConfigName("balancepush")
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath(".")
+		viper.AddConfigPath("../balancepush_bank_checker/")
+		viper.AddConfigPath("$HOME/config/")
+		viper.AddConfigPath("/usr/local/etc/")
+		viper.AddConfigPath("/etc/")
+		err := viper.ReadInConfig()
+		if err != nil {
+			logger.Println(err)
+			logger.Fatal("Cannot read config file. File may not exist, or be in the wrong format.")
+		}
+		err = viper.Unmarshal(&config)
+		if err != nil {
+			logger.Fatal("Cannot read config file. File may be in the wrong format.")
+		}
+	})
+
+	if logger.GetLevel() == logger.TraceLevel {
+		pc, _, _, ok := runtime.Caller(1)
+		details := runtime.FuncForPC(pc)
+		if ok && details != nil {
+			logger.Tracef("Returning config to %s", details.Name())
+		}
+	}
+
+	return config
+}
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/forquare/balancepush/config"
+	"github.com/forquare/balancepush/gocardless"
+	"github.com/gregdel/pushover"
+	"log"
+	"strings"
+)
+
+func main() {
+	c := config.GetConfig()
+
+	client, err := gocardless.NewGoCardlessClient(c.GoCardless.Credentials.SecretID, c.GoCardless.Credentials.SecretKey)
+	if err != nil {
+		log.Fatalf("Error creating client: %v", err)
+	}
+
+	l := len(c.GoCardless.Bank.Accounts)
+	msg := make([]string, l)
+	for i, account := range c.GoCardless.Bank.Accounts {
+		account.Balance, account.Currency, account.CurrencySymbol, err = client.GetAccountBalance(account.ID, account.BalanceType)
+		if err != nil {
+			log.Fatalf("Could not get balance: %v", err)
+		}
+
+		m := fmt.Sprintf("%s: %s%.2f", account.Name, account.CurrencySymbol, account.Balance)
+		msg[i] = m
+	}
+
+	app := pushover.New(c.Pushover.Tokens.App)
+	recipient := pushover.NewRecipient(c.Pushover.Tokens.User)
+	message := pushover.NewMessage(strings.Join(msg, "\n"))
+	response, err := app.SendMessage(message, recipient)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(response)
+}
